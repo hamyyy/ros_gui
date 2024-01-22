1a3aef66e9a2341d1c874356e1d3ea276284934e
hamyy <oudy_1999@hotmail.com>
2024-01-22 13:40:12 +0300

    removed build and .vscode folders

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hamoudy/ros_ws/src/ros_gui
-# Build directory: /home/hamoudy/ros_ws/src/ros_gui/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
